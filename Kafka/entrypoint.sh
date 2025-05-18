48a111d6ecc7fc578492b7da160ae9ae59d27250
cfy1310 <abdul.memon@tuhh.de>
2025-05-18 19:38:54 +0200

    data provider implementation

---
+++ Kafka/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Start Zookeeper
+echo "Starting Zookeeper..."
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+ZOOKEEPER_PID=$!
+sleep 10 # Wait for Zookeeper to start
+
+# Start Kafka
+echo "Starting Kafka..."
+bin/kafka-server-start.sh config/server.properties &
+KAFKA_PID=$!
+sleep 10 # Wait for Kafka to start
+
+# Run the Python script
+echo "Running data provider..."
+python3 /data_provider.py
+
+# Wait for Kafka and Zookeeper processes to finish
+wait $ZOOKEEPER_PID
+wait $KAFKA_PID
